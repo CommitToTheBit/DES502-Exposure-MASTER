1f4096b42414ad9db958d34481aa9d5156997937
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-03-15 18:31:20 +0000

    Squashed commit of the following:
    commit 3a3f24599695015f69901ecba7d44f9f9f487266
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Wed Mar 15 18:30:54 2023 +0000
    
        Copied relevant functionality into main settings menu
    
    commit cfd4d5df1c5f7df8769c84dc9eec95dd25a54019
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Wed Mar 15 18:19:04 2023 +0000
    
        Established 'two slider' system for volume (though only in the pause menu so far!)
    
    commit e63927c6a6b7480e2184007d390212c75791cbf5
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Wed Mar 15 16:42:36 2023 +0000
    
        Major game instance refactoring, to better access data...
    
    commit 71c467668dc83c20364044fef3528630b2a2b9bc
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Wed Mar 15 15:21:01 2023 +0000
    
        (Rudimentary) Prototype of a volume slider!
    
    commit 57edf5a0a8fffb15ef6fa70abd25487f8df728c3
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Wed Mar 15 14:51:58 2023 +0000
    
        CHECKPOINT: Now able to set mixes dynamically!
    
        Key tutorial on getting/setting: ~40mins in to https://www.youtube.com/watch?v=BV5l_7-w7cg&t=2942s (Dan Reynolds)
    
    commit 022f7ce4afaf5ab2f9504f225b2b1f37bef5d2b6
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Tue Mar 14 17:40:21 2023 +0000
    
        Minor change while troubleshooting rock UVs...
    
    commit 9d0818aa88ae2bcabfe38d62374352f47923b23c
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Tue Mar 14 16:31:29 2023 +0000
    
        Found a potential tutorial - in the meanwhile, messing about with the game mode BPs!
    
    commit c973b9679c97e92ea94eb12d8d6c3a0cad47836f
    Author: CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
    Date:   Tue Mar 14 16:14:06 2023 +0000
    
        Added a bus for master SFX volume...
    
        Now to figure out how to modify it dynamically!

--- DES502_Build/Source/DES502_Build/Private/DESGameInstance.cpp
+++ DES502_Build/Source/DES502_Build/Private/DESGameInstance.cpp
@@ -7,60 +7,50 @@ void UDESGameInstance::Init()
 	// BUGFIX: This is what makes the calls that set up widgets!
 	Super::Init();
 
-	// Try loading data...
-	GameData = Cast<UDESSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlot, 0));
-	if (GameData)
+	// Load any saved data...
+	LoadGameData(true); // DEBUG: Set to true to reset GameData on start...
+}
+
+void UDESGameInstance::SaveGameData() 
+{
+	if (!GameData)
 		return;
+
+	UGameplayStatics::SaveGameToSlot(GameData, SaveSlot, 0);
+}
+
+void UDESGameInstance::LoadGameData(bool resetGameData)
+{
+	// Try loading data...
+	if (!resetGameData)
+	{
+		GameData = Cast<UDESSaveGame>(UGameplayStatics::LoadGameFromSlot(SaveSlot, 0));
+		if (GameData)
+			return;
+	}
 
 	// If this fails, create new game data...
 	GameData = Cast<UDESSaveGame>(UGameplayStatics::CreateSaveGameObject(UDESSaveGame::StaticClass()));
+
+	// Settings Variables...
+	GameData->SettingsMasterVolume = 1.0f;
+	GameData->SettingsMasterMute = false;
+
+	GameData->SettingsMusicVolume = 1.0f;
+	GameData->SettingsMusicMute = false;
+
+	GameData->SettingsSFX_Volume = 1.0f;
+	GameData->SettingsSFX_Mute = false;
 
 	// Player Variables...
 	GameData->PlayerPosition = FVector(0.0, 0.0, 0.0);
 	GameData->PlayerCameraRotation = FRotator(0.0, 0.0, 0.0);
 
-	// Polaroid Variables...
-	GameData->PolaroidAmmo = 27;
-
-	// Crawl Space Variables...
 	GameData->bCrawling = false;
 	GameData->CrawlDistance = 0.0f;
 
-	UGameplayStatics::SaveGameToSlot(GameData, SaveSlot, 0);
-}
-
-void UDESGameInstance::SaveGameData(
-	float SettingsMasterVolume,
-	float SettingsMusicVolume,
-	float SettingsSFXVolume,
-	FVector PlayerPosition,
-	FRotator PlayerCameraRotation,
-	int PolaroidAmmo,
-	bool bCrawling,
-	float CrawlDistance
-) 
-{
-	if (!GameData)
-		return;
-
-	// Settings Variables...
-	// FIXME: Add volume controls here!
-
-	// Player Variables...
-	GameData->PlayerPosition = PlayerPosition;
-	GameData->PlayerCameraRotation = PlayerCameraRotation,
-
 	// Polaroid Variables...
-	GameData->PolaroidAmmo = PolaroidAmmo;
-
-	// Crawl Space Variables...
-	GameData->bCrawling = bCrawling;
-	GameData->CrawlDistance = CrawlDistance;
+	GameData->PolaroidAmmo = 27;
 
 	UGameplayStatics::SaveGameToSlot(GameData, SaveSlot, 0);
-}
-
-UDESSaveGame* UDESGameInstance::LoadGameData()
-{
-	return (GameData) ? GameData : nullptr;
 }
