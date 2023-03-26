52312e4e3768a08fe373bc813610333570058bb7
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-03-26 20:31:47 +0100

    FIXME: C++ class not showing in content browser...

---
+++ DES502_Build/Source/DES502_Build/Private/DES_EncyclopediaEntryProgress.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "DES_EncyclopediaEntryProgress.h"
+
+DES_EncyclopediaEntryProgress::DES_EncyclopediaEntryProgress()
+{
+}
+
+DES_EncyclopediaEntryProgress::~DES_EncyclopediaEntryProgress()
+{
+}
