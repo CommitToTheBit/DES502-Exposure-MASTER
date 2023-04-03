2418930b17bbdbd17a85120f175b8ecbb822a0ee
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-04-03 13:04:12 +0100

    Confirming previous branch is working; planning out camera recognition MVP

--- DES502_Build/Source/DES502_Build/Public/DESCurio.h
+++ DES502_Build/Source/DES502_Build/Public/DESCurio.h
@@ -10,6 +10,10 @@ class DES502_BUILD_API ADESCurio : public AActor
 	GENERATED_BODY()
 	
 public:	
-	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category="Scoring")
-		float CurioScore;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recognition")
+		FString ID = "";
+
+	// NB: Will we assign a 'value' to each photo composition? Could get out of hand...
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Recognition")
+	//	float Score;
 };
