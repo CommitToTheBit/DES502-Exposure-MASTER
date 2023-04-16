efe4b4472f50a508b0a6b54000ed27df662cb2d9
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-04-17 00:35:23 +0100

    Successfully compiling work on TreeVertices - even if there are unforeseen errors?

--- DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
+++ DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
@@ -95,7 +95,96 @@ void ADES_L_System::InitializeSentence(float Seed, TArray<FDES_L_Module> Axiom, 
 
 void ADES_L_System::InitializeTree(float Seed, float Rotation, FVector2D Anchoring)
 {
+	LSeed = Seed;
+	LRotation = Rotation;
+	LScale = 1.0f;
+	LDepth = 0.0f;
 
+	Time = 0.0f;
+	Intensity = 0.0f;
+
+	SeedVertices = TArray<FDES_SeedVertex>();
+	SeedVertices.Add(FDES_SeedVertex());
+	SeedVertices[0].Parent = 0;
+	SeedVertices[0].Transform = FTransform(FRotator(0.0f, Rotation, 0.0f));
+	SeedVertices[0].Position = FVector(0.0f, 0.0f, 0.0f);
+	SeedVertices[0].Depth = 0.0f;
+
+	int ParentIndex = 0;
+	TArray<int> ParentIndices = TArray<int>();
+
+	FTransform LocalTransform = FTransform::Identity;
+
+	// STEP 1: Create branching structure...
+	int ChildIndex = 1;
+	for (FDES_L_Module L_Module : Sentence)
+	{
+		if (L_Module.Letter == "[")
+		{
+			ParentIndices.Add(ParentIndex);
+		}
+		else if (L_Module.Letter == "]" && ParentIndices.Num() > 0)
+		{
+			ParentIndex = ParentIndices.Pop();
+		}
+		else if (L_Module.Letter != "]")
+		{
+			LocalTransform *= FTransform(FRotator(0.0f, L_Module.StaticRotation, 0.0f)); // NB: Note the order of operations!
+
+			if (L_Module.StaticLength == 0.0f)
+				continue;
+
+			LocalTransform *= FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f));
+
+			SeedVertices.Add(FDES_SeedVertex());
+			SeedVertices[ChildIndex].Parent = ParentIndex;
+			SeedVertices[ChildIndex].Transform = SeedVertices[ParentIndex].Transform * LocalTransform;
+			SeedVertices[ChildIndex].Position = FVector(0.0f, 0.0f, 0.0f); 
+			SeedVertices[ChildIndex].Transform.TransformVector(SeedVertices[ChildIndex].Position);
+			SeedVertices[ChildIndex].Depth = SeedVertices[ParentIndex].Depth + L_Module.StaticLength;
+
+			ChildIndex = SeedVertices.Num();
+			ParentIndex = ChildIndex - 1;
+			LocalTransform = FTransform::Identity;
+		}
+	}
+
+	// STEP 2: Calculate bounds...
+	FVector2D Minima = FVector2D(0.0f, 0.0f);
+	FVector2D Maxima = FVector2D(0.0f, 0.0f);
+	for (int i = 0; i < SeedVertices.Num(); i++)
+	{
+		if (SeedVertices[i].Position.X < Minima.X)
+			Minima.X = SeedVertices[i].Position.X;
+		else if (SeedVertices[i].Position.X > Maxima.X)
+			Maxima.X = SeedVertices[i].Position.X;
+
+		if (SeedVertices[i].Position.Y < Minima.Y)
+			Minima.Y = SeedVertices[i].Position.Y;
+		else if (SeedVertices[i].Position.Y > Maxima.Y)
+			Maxima.Y = SeedVertices[i].Position.Y;
+	}
+
+	FVector2D Delta = Maxima - Minima;
+	float MaxDelta = std::max(Delta.X, Delta.Y);
+	if (MaxDelta > 0.0f) // NB: In the case of a point, scaling won't matter!
+		LScale /= MaxDelta;
+
+	// STEP 3: Rescale... 
+	float XBorder = Anchoring.X * (1.0f - Delta.X / MaxDelta);
+	float YBorder = Anchoring.Y * (1.0f - Delta.Y / MaxDelta);
+
+	for (int i = 0; i < SeedVertices.Num(); i++)
+	{
+		SeedVertices[i].Position.X = XBorder + (SeedVertices[i].Position.X - Minima.X) / MaxDelta;
+		SeedVertices[i].Position.Y = YBorder + (SeedVertices[i].Position.Y - Minima.Y) / MaxDelta;
+
+		SeedVertices[i].Depth *= LScale;
+		LDepth = std::max(SeedVertices[i].Depth, LDepth);
+	}
+
+	// STEP 4: Instantaneously 'update' TreeVertices...
+	UpdateTree(0.0f, 0.0f);
 }
 void ADES_L_System::InitializeMesh(UProceduralMeshComponent* Mesh)
 {
@@ -109,7 +198,79 @@ void ADES_L_System::Update(float DeltaTime, float DeltaIntensity)
 
 void ADES_L_System::UpdateTree(float DeltaTime, float DeltaIntensity)
 {
+	FRandomStream rng = FRandomStream(LSeed);
 
+	Time += DeltaTime;
+	Intensity += DeltaIntensity;
+	Intensity = std::max(0.0f, std::min(Intensity, 1.0f));
+
+	TreeVertices = TArray<FDES_TreeVertex>();
+	TreeVertices.Add(FDES_TreeVertex());
+	TreeVertices[0].Parent = 0;
+	TreeVertices[0].Transform = FTransform(SeedVertices[0].Position) * FTransform(FRotator(0.0f, LRotation, 0.0f)); // NB: Assumes we've initialised m_seedVertices!
+	TreeVertices[0].Position = FVector(0.0f, 0.0f, 0.0f);
+	TreeVertices[0].Transform.TransformVector(TreeVertices[0].Position);
+	TreeVertices[0].Width = 0.0f;
+	TreeVertices[0].Degree = 0;
+
+	int ParentIndex = 0;
+	TArray<int> ParentIndices = TArray<int>();
+
+	FTransform LocalTransform = FTransform::Identity;
+
+	// STEP 1: Create branching structure...
+	int ChildIndex = 1;
+	float StaticLength, PeriodicLength, StaticRotation, PeriodicRotation, StaticWidth, PeriodicWidth, Period, Oscillation, Scale;
+	for (FDES_L_Module L_Module : Sentence)
+	{
+		if (L_Module.Letter == "[")
+		{
+			ParentIndices.Add(ParentIndex);
+		}
+		else if (L_Module.Letter == "]" && ParentIndices.Num() > 0)
+		{
+			ParentIndex = ParentIndices.Pop();
+		}
+		else if (L_Module.Letter != "]")
+		{
+			Period = L_Module.Period + rng.FRandRange(0.0f, std::max(L_Module.Aperiodicity, 0.0f));
+			Oscillation = (L_Module.Period > 0.0f) ? cos(2.0f * PI * (Time / Period) + (L_Module.Synchronisation + rng.FRandRange(0.0f, L_Module.Asynchronicity))) : 0.0f;
+			StaticRotation = L_Module.StaticRotation + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomStaticRotation;
+			PeriodicRotation = Oscillation * (L_Module.PeriodicRotation + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomPeriodicRotation);
+			LocalTransform *= FTransform(FRotator(0.0f, StaticRotation + PeriodicRotation, 0.0f));
+
+			if (L_Module.StaticLength == 0.0f)
+				continue;
+
+			Scale = (SeedVertices[ChildIndex].Depth > 0.0f) ? LScale * std::max(0.0f, std::min((1.0f - LDepth / SeedVertices[ChildIndex].Depth) + (LDepth / SeedVertices[ChildIndex].Depth) * Intensity, 1.0f)) : 0.0f;
+
+			Period = L_Module.Period + rng.FRandRange(0.0f, std::max(L_Module.Aperiodicity, 0.0f));
+			Oscillation = (L_Module.Period > 0.0f) ? cos(2.0f * PI * (Time / Period) + (L_Module.Synchronisation + rng.FRandRange(0.0f, L_Module.Asynchronicity))) : 0.0f;
+			StaticLength = L_Module.StaticLength + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomStaticLength;
+			PeriodicLength = Oscillation * (L_Module.PeriodicLength + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomPeriodicLength);
+			LocalTransform *= FTransform(FVector(Scale * (StaticLength + PeriodicLength), 0.0f, 0.0f));
+
+			Period = L_Module.Period + rng.FRandRange(0.0f, std::max(L_Module.Aperiodicity, 0.0f));
+			Oscillation = (L_Module.Period > 0.0f) ? cos(2.0f * PI * (Time / Period) + (L_Module.Synchronisation + rng.FRandRange(0.0f, L_Module.Asynchronicity))) : 0.0f;
+			StaticWidth = L_Module.StaticWidth + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomStaticWidth;
+			PeriodicWidth = Oscillation * (L_Module.PeriodicWidth + rng.FRandRange(-1.0f, 1.0f) * L_Module.RandomPeriodicWidth);
+
+			TreeVertices.Add(FDES_TreeVertex());
+			TreeVertices[ChildIndex].Parent = ParentIndex;
+			TreeVertices[ChildIndex].Transform = TreeVertices[ParentIndex].Transform * LocalTransform;
+			TreeVertices[ChildIndex].Position = FVector(0.0f, 0.0f, 0.0f);
+			TreeVertices[ChildIndex].Transform.TransformVector(TreeVertices[ChildIndex].Position);
+			TreeVertices[ChildIndex].Width = Scale * (StaticWidth + PeriodicWidth);
+			TreeVertices[ChildIndex].Degree = 1;
+
+			TreeVertices[ParentIndex].Width = std::max(TreeVertices[ChildIndex].Width, TreeVertices[ParentIndex].Width);
+			TreeVertices[ParentIndex].Degree++;
+
+			ChildIndex = TreeVertices.Num();
+			ParentIndex = ChildIndex - 1;
+			LocalTransform = FTransform::Identity;
+		}
+	}
 }
 
 void ADES_L_System::AddProductionRule(FString Letter, FDES_ProductionRule ProductionRule)
