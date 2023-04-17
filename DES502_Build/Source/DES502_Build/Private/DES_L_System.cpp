e935d706843ef0ebce5788be945762f7c6bea11b
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-04-17 19:29:25 +0100

    PROGRESS: Set a constant width, mesh is now visible!

--- DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
+++ DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
@@ -139,7 +139,7 @@ void ADES_L_System::InitializeTree(float Seed, float Rotation, FVector2D Anchori
 			SeedVertices.Add(FDES_SeedVertex());
 			SeedVertices[ChildIndex].Parent = ParentIndex;
 			SeedVertices[ChildIndex].Transform = SeedVertices[ParentIndex].Transform * LocalTransform;
-			SeedVertices[ChildIndex].Position = SeedVertices[ChildIndex].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
+			SeedVertices[ChildIndex].Position = SeedVertices[ChildIndex].Transform.TransformPosition(FVector(0.0f, 0.0f, 0.0f));
 			SeedVertices[ChildIndex].Depth = SeedVertices[ParentIndex].Depth + L_Module.StaticLength;
 
 			ChildIndex = SeedVertices.Num();
@@ -147,7 +147,8 @@ void ADES_L_System::InitializeTree(float Seed, float Rotation, FVector2D Anchori
 			LocalTransform = FTransform::Identity;
 
 			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f)), FVector(0.0f, 0.0f, 0.0f)).ToString());
-			GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, (FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f)).TransformVectorNoScale(FVector(1.0f, 0.0f, 0.0f))).ToString());
+			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, SeedVertices[ParentIndex].Position.ToString());
+			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, (FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f)).TransformPosition(FVector(0.0f, 0.0f, 0.0f))).ToString());
 			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, SeedVertices[ParentIndex].Position.ToString());
 		}
 	}
@@ -208,13 +209,13 @@ void ADES_L_System::InitializeMesh(UProceduralMeshComponent* Mesh)
 		TreeVertex = TreeVertices[i];
 		ParentVertex = TreeVertices[TreeVertex.Parent];
 
-		Orthogonal = FTransform(FRotator(0.0f, PI/2.0f, 0.0f)).TransformVector(TreeVertex.Position - ParentVertex.Position);
+		Orthogonal = FTransform(FRotator(0.0f, PI/2.0f, 0.0f)).TransformPosition(TreeVertex.Position - ParentVertex.Position);
 		Orthogonal.Normalize();
 
-		Vertices[4 * i] = ParentVertex.Position + ParentVertex.Width * Orthogonal;
-		Vertices[4 * i + 1] = ParentVertex.Position - ParentVertex.Width * Orthogonal;
-		Vertices[4 * i + 2] = TreeVertex.Position - TreeVertex.Width * Orthogonal;
-		Vertices[4 * i + 3] = TreeVertex.Position + TreeVertex.Width * Orthogonal;
+		Vertices[4 * i] = FVector(ParentVertex.Position + ParentVertex.Width * Orthogonal);
+		Vertices[4 * i + 1] = FVector(ParentVertex.Position - ParentVertex.Width * Orthogonal);
+		Vertices[4 * i + 2] = FVector(TreeVertex.Position - TreeVertex.Width * Orthogonal);
+		Vertices[4 * i + 3] = FVector(TreeVertex.Position + TreeVertex.Width * Orthogonal);
 
 		Indices[12 * i] = 4 * TreeVertex.Parent + 2;
 		Indices[12 * i + 1] = 4 * TreeVertex.Parent + 3;
@@ -232,16 +233,23 @@ void ADES_L_System::InitializeMesh(UProceduralMeshComponent* Mesh)
 		Indices[12 * i + 10] = 4 * i + 2;
 		Indices[12 * i + 11] = 4 * i + 3;
 
-		//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, FString::FromInt(i));
+		if (i == 1)
+			GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, FString::FromInt(TreeVertex.Width));
 	}
 
-	/*Vertices[0] = FVector(1.0f, 1.0f, 0.0f);
-	Vertices[1] = FVector(1.0f, -1.0f, 0.0f);
-	Vertices[2] = FVector(-1.0f, -1.0f, 0.0f);
+	//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, Vertices[0].ToString());
 
-	Indices[0] = 0;
-	Indices[1] = 1;
-	Indices[2] = 2;*/
+	//Vertices[0] = FVector(1.0f, 1.0f, 0.0f);
+	//Vertices[1] = FVector(1.0f, -1.0f, 0.0f);
+	//Vertices[2] = FVector(-1.0f, -1.0f, 0.0f);
+
+	///Indices[0] = 0;
+	//Indices[1] = 0;
+	//Indices[2] = 0;
+
+	//Indices[3] = 0;
+	//Indices[4] = 2;
+	//Indices[5] = 1;
 
 	LMesh->CreateMeshSection_LinearColor(0, Vertices, Indices, TArray<FVector>(), TArray<FVector2D>(), TArray<FLinearColor>(), TArray<FProcMeshTangent>(), true);
 	LMesh->SetMeshSectionVisible(0, true);
@@ -264,7 +272,7 @@ void ADES_L_System::UpdateTree(float DeltaTime, float DeltaIntensity)
 	TreeVertices.Add(FDES_TreeVertex());
 	TreeVertices[0].Parent = 0;
 	TreeVertices[0].Transform = FTransform(SeedVertices[0].Position) * FTransform(FRotator(0.0f, LRotation, 0.0f)); // NB: Assumes we've initialised m_seedVertices!
-	TreeVertices[0].Position = TreeVertices[0].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
+	TreeVertices[0].Position = TreeVertices[0].Transform.TransformPosition(FVector(0.0f, 0.0f, 0.0f));
 	TreeVertices[0].Width = 0.0f;
 	TreeVertices[0].Degree = 0;
 
@@ -313,8 +321,8 @@ void ADES_L_System::UpdateTree(float DeltaTime, float DeltaIntensity)
 			TreeVertices.Add(FDES_TreeVertex());
 			TreeVertices[ChildIndex].Parent = ParentIndex;
 			TreeVertices[ChildIndex].Transform = TreeVertices[ParentIndex].Transform * LocalTransform;
-			TreeVertices[ChildIndex].Position = TreeVertices[ChildIndex].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
-			TreeVertices[ChildIndex].Width = Scale * (StaticWidth + PeriodicWidth);
+			TreeVertices[ChildIndex].Position = TreeVertices[ChildIndex].Transform.TransformPosition(FVector(0.0f, 0.0f, 0.0f));
+			TreeVertices[ChildIndex].Width = 1.0f;// Scale* (StaticWidth + PeriodicWidth);
 			TreeVertices[ChildIndex].Degree = 1;
 
 			TreeVertices[ParentIndex].Width = std::max(TreeVertices[ChildIndex].Width, TreeVertices[ParentIndex].Width);
