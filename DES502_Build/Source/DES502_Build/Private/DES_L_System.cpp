64118ed537db6f076a5dd0c772050bc678069d8e
CommitToTheBit <115366568+CommitToTheBit@users.noreply.github.com>
2023-04-17 17:50:38 +0100

    FIXME: Why aren't transforms doing anything?

--- DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
+++ DES502_Build/Source/DES502_Build/Private/DES_L_System.cpp
@@ -139,13 +139,16 @@ void ADES_L_System::InitializeTree(float Seed, float Rotation, FVector2D Anchori
 			SeedVertices.Add(FDES_SeedVertex());
 			SeedVertices[ChildIndex].Parent = ParentIndex;
 			SeedVertices[ChildIndex].Transform = SeedVertices[ParentIndex].Transform * LocalTransform;
-			SeedVertices[ChildIndex].Position = FVector(0.0f, 0.0f, 0.0f); 
-			SeedVertices[ChildIndex].Transform.TransformVector(SeedVertices[ChildIndex].Position);
+			SeedVertices[ChildIndex].Position = SeedVertices[ChildIndex].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
 			SeedVertices[ChildIndex].Depth = SeedVertices[ParentIndex].Depth + L_Module.StaticLength;
 
 			ChildIndex = SeedVertices.Num();
 			ParentIndex = ChildIndex - 1;
 			LocalTransform = FTransform::Identity;
+
+			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f)), FVector(0.0f, 0.0f, 0.0f)).ToString());
+			GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, (FTransform(FVector(L_Module.StaticLength, 0.0f, 0.0f)).TransformVectorNoScale(FVector(1.0f, 0.0f, 0.0f))).ToString());
+			//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, SeedVertices[ParentIndex].Position.ToString());
 		}
 	}
 
@@ -205,8 +208,7 @@ void ADES_L_System::InitializeMesh(UProceduralMeshComponent* Mesh)
 		TreeVertex = TreeVertices[i];
 		ParentVertex = TreeVertices[TreeVertex.Parent];
 
-		Orthogonal = TreeVertex.Position - ParentVertex.Position;
-		FTransform(FRotator(0.0f, PI/2.0f, 0.0f)).TransformVector(Orthogonal);
+		Orthogonal = FTransform(FRotator(0.0f, PI/2.0f, 0.0f)).TransformVector(TreeVertex.Position - ParentVertex.Position);
 		Orthogonal.Normalize();
 
 		Vertices[4 * i] = ParentVertex.Position + ParentVertex.Width * Orthogonal;
@@ -229,9 +231,20 @@ void ADES_L_System::InitializeMesh(UProceduralMeshComponent* Mesh)
 		Indices[12 * i + 9] = 4 * i;
 		Indices[12 * i + 10] = 4 * i + 2;
 		Indices[12 * i + 11] = 4 * i + 3;
+
+		//GEngine->AddOnScreenDebugMessage(0, 15.0f, FColor::Magenta, FString::FromInt(i));
 	}
 
-	LMesh->CreateMeshSection_LinearColor(0, Vertices, Indices, TArray<FVector>(), TArray<FVector2D>(), TArray<FLinearColor>(), TArray<FProcMeshTangent>(), false);
+	/*Vertices[0] = FVector(1.0f, 1.0f, 0.0f);
+	Vertices[1] = FVector(1.0f, -1.0f, 0.0f);
+	Vertices[2] = FVector(-1.0f, -1.0f, 0.0f);
+
+	Indices[0] = 0;
+	Indices[1] = 1;
+	Indices[2] = 2;*/
+
+	LMesh->CreateMeshSection_LinearColor(0, Vertices, Indices, TArray<FVector>(), TArray<FVector2D>(), TArray<FLinearColor>(), TArray<FProcMeshTangent>(), true);
+	LMesh->SetMeshSectionVisible(0, true);
 }
 
 void ADES_L_System::Update(float DeltaTime, float DeltaIntensity)
@@ -251,8 +264,7 @@ void ADES_L_System::UpdateTree(float DeltaTime, float DeltaIntensity)
 	TreeVertices.Add(FDES_TreeVertex());
 	TreeVertices[0].Parent = 0;
 	TreeVertices[0].Transform = FTransform(SeedVertices[0].Position) * FTransform(FRotator(0.0f, LRotation, 0.0f)); // NB: Assumes we've initialised m_seedVertices!
-	TreeVertices[0].Position = FVector(0.0f, 0.0f, 0.0f);
-	TreeVertices[0].Transform.TransformVector(TreeVertices[0].Position);
+	TreeVertices[0].Position = TreeVertices[0].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
 	TreeVertices[0].Width = 0.0f;
 	TreeVertices[0].Degree = 0;
 
@@ -301,8 +313,7 @@ void ADES_L_System::UpdateTree(float DeltaTime, float DeltaIntensity)
 			TreeVertices.Add(FDES_TreeVertex());
 			TreeVertices[ChildIndex].Parent = ParentIndex;
 			TreeVertices[ChildIndex].Transform = TreeVertices[ParentIndex].Transform * LocalTransform;
-			TreeVertices[ChildIndex].Position = FVector(0.0f, 0.0f, 0.0f);
-			TreeVertices[ChildIndex].Transform.TransformVector(TreeVertices[ChildIndex].Position);
+			TreeVertices[ChildIndex].Position = TreeVertices[ChildIndex].Transform.TransformVector(FVector(0.0f, 0.0f, 0.0f));
 			TreeVertices[ChildIndex].Width = Scale * (StaticWidth + PeriodicWidth);
 			TreeVertices[ChildIndex].Degree = 1;
 
