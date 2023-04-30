7ceffc64207aaab8e8a97f7d92a4e6fec4cdeee8
abertayray <2104143@uad.ac.uk>
2023-04-30 19:16:45 +0100

    Cutscene v2, now with audio!

--- DES502_Build/Saved/VideoCaptures/SequenceMaster.bat
+++ DES502_Build/Saved/VideoCaptures/SequenceMaster.bat
@@ -1,1 +1,1 @@
-rv -nomb -fullscreen -noBorders -fps 30.000000 shot0010_01.avi shot0020_01.avi
+rv -nomb -fullscreen -noBorders -fps 60.000000 shot0010_01.avi shot0020_01.avi shot0030_01.avi shot0040_01.avi shot0050_01.avi shot0060_01.avi
